b616658368416a0087086e18de59ffebf009d133
hongs <shh0429@kakao.com>
2023-05-23 22:02:50 +0900

    다양한 방법의 request의 parameter 받는 방법 공부

--- Bt11_RequestParam/src/main/java/com/lec/spring/controller/HomeController.java
+++ Bt11_RequestParam/src/main/java/com/lec/spring/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.lec.spring.controller;
 
+import com.lec.spring.domain.Write;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +28,7 @@ public class HomeController {
         String num = request.getParameter("num"); // 10만 넘어왔다.. 난 10 20 30 을 넘겨줫는데,
         System.out.println("num : " + num);
 
-        String[] arrNum = request.getParameterValues("num");
+        String[] arrNum = request.getParameterValues("num");        // >> Servlet 방식
         System.out.println("arrNum : " + Arrays.toString(arrNum));
         // url :  http://localhost:8081/member/delete?id=hello
         // getParameter(param age) <-- 없는 parameter 의 경우, null을 리턴한다!
@@ -44,7 +47,7 @@ public class HomeController {
     public void registMember() {
     }
 
-    @RequestMapping("/member/regOk")
+    @RequestMapping("/member/regOk")  // RequestMapping 은 어떤 request Method도  받을 수 있다.
     public void regOk(HttpServletRequest request, Model model) {
 
         String methodName = request.getMethod();
@@ -55,4 +58,159 @@ public class HomeController {
         model.addAttribute("name", name);
         System.out.println("name : " + name);
     }
-}
+
+    // 특정 request method(들) 에서만 동작하는 url 매핑
+    @RequestMapping(value = "/member/regOk2", method = {RequestMethod.GET, RequestMethod.PUT})
+    public String regOk2(HttpServletRequest request, Model model) {
+
+        String methodName = request.getMethod();
+
+        System.out.println("/member/regOk2 requested " + methodName);
+
+        String name = request.getParameter("name");
+        model.addAttribute("name", name);
+        System.out.println("name : " + name);
+
+        return "member/regOk";
+    }
+
+    // 단일 request method 에서만 동작하는 핸들러는
+    // @GetMapping(..), @PostMapping(..), @PutMapping(...)  등을 사용하는게 간편하다
+
+    // parameter name 값과 동일한 이름의 변수를 핸들러에 지정해서 받아오기
+    @RequestMapping("/member/find1")
+//    public void find1(String id, String name, Model model) {      >> 매개변수 순서 달라도 된다~
+    public void find1(String name, Model model, String id) {
+        // 원래는 이렇게 했엇는데~~~~
+//        String name = request.getParameter("id");
+//        String name = request.getParameter("name");
+
+        System.out.println("member/find1 : id=" + id + ", name=" + name);
+        // model 객체에 담아서 주는 것 말고도 thymeleaf 자체에서도 알아서 해줄수 있다. >> ${param.~~}
+//        model.addAttribute("id", id);
+//        model.addAttribute("name", name);
+    }
+
+
+    // 숫자 타입은 바로 parsing 하여 받을 수 있다.
+    @RequestMapping("/member/find2")
+    //                   여기 바뀜
+//    public void find2(double id, String name, Model model) { // primitive 타입인데, parameter 에 없는 경우, 혹은 parsing 불가능하면 에러 발생
+    public void find2(Double id, String name, Model model) { // wrapper 타입이면, parameter 없으면 null 값으로 받아옴. parsing 불가하면 에러
+        //                  ↑          ↑        >> request.get~ 이 아니라 위의 방법대로하는 것을 binding이라고 한다.
+        // 위 과정을 binding 한다 라고 한다.
+        // parameter "name"-"value"  -->  Java 변수, 객체
+
+
+        System.out.println("member/find2 : id=" + id + ", name=" + name);
+        // 만약에 id 값이 double 형태로 변환이 안된다면? >> 에러를 띄운다
+        // id 값이 double로 형변환 가능한 것들로만 해야 한다~
+    }
+
+
+    // 동일한 name 의 parameter(들) --> '배열' 매개변수로 받을수 있다.
+    // String[] arrNum = request.getParameterValues("num");        // >> Servlet 방식
+    @RequestMapping("/member/find3")
+    public void find3(int[] id, String[] name, Model model) {
+        System.out.println("member/find3 : id=" + Arrays.toString(id) + ", name=" + Arrays.toString(name));
+
+
+    }
+
+    // 만약 request parameter 의 name 과 매개변수가 같을수 없는 상황이면
+    // @RequestParam 애노테이션 사용
+    @RequestMapping("/member/find4")
+    public void find4(
+            @RequestParam("id") String userid,  // "id" 란 name 의 parameter 값을 userid 매개변수에 binding 해준다
+            // @RequestParam 을 쓰면, parameter를 안넘겨주면, 이전처럼 null이 아니라, 에러발생
+            @RequestParam("name") String username
+    ) {
+        System.out.println("member/find4 : id=" + userid + ", name=" + username);
+    }
+    // 위의 경우 id 값이 없거나 변환 불가능하면 에러 발생한다. (왜냐하면 required=true 이기 때문이다)
+    // @RequestParam(value="test", required=false, defaultValue="0") 을 이용하면 가능하긴 하다.✨✨✨✨
+
+
+    // 또한, @RequestParam 과 Map<name, value> 을 사용하면 된다.
+    @RequestMapping("/member/find5")
+    public void find5(@RequestParam Map<String, String> map) {
+        System.out.println("member/find5 : " + map);
+    }
+
+    //--------------------------------------------------------------
+    // 커맨드 객체 (Command Object) 사용
+
+    // 게시글 등록 form
+    @GetMapping("/board/write")
+    public void writeBoard() {
+    }
+
+    // 기존의 방식대로라면,
+    // 매 parameter 들을 매개변수화 하는 것은 힘들다.
+//    @PostMapping("/board/writeOk")
+//    public void writeOkBoard(
+//            String name,
+//            String subject,
+//            String content
+//    ) {}
+
+    // 커맨드 객체 사용
+    // 코드 작업량이 매우 줄어든다.
+
+    // 커맨드 객체는 객체타입명으로 Model attribute 추가 된다. (소문자로)
+    // Model attribute name 을 바꿀경우 @ModelAttribute 로 지정
+    @PostMapping("/board/writeOk")
+    public void writeOkBoard(
+            @ModelAttribute("DTO") Write w
+//            Write k
+    ) {
+        // write 객체가 넘어 온 parameter 를 binding 하고 있다~
+        System.out.println("/board/writeOk(w) : " + w);
+//        System.out.println("/board/writeOk(k) : " + k);
+    }
+
+    //--------------------------------------
+    // @PathVariable 사용
+    @RequestMapping("/board/writePath/{name}/{subject}/{k3}")
+    public String writePathBoard(
+            @PathVariable String name,
+            @PathVariable String subject,
+            @PathVariable(name = "k3") String content,
+            Model model
+    ) {
+        System.out.println("/board/writePath/ : name = " + name + ", subject = " + subject + ", content = " + content);
+        model.addAttribute("name", name);
+        model.addAttribute("subject", subject);
+        model.addAttribute("content", content);
+        return "/board/writepath";
+    }
+
+    // ----------------------------------------------------
+    // @ResponseBody : View 를 사용하지 않고 직접 response
+    @RequestMapping("/board/update")
+    @ResponseBody
+    public String boardUpdate() {
+        StringBuffer buff = new StringBuffer();
+        buff.append("<h1>Hello Srping</h1>");
+        buff.append("<h3>1.  Request Parameter</h3>");
+
+        return buff.toString();
+    }
+
+
+    @RequestMapping("/board/detail")
+    @ResponseBody
+    public Write boardDetail(){
+        // Object 를 response 하면 JSON 으로 변환되어 response 된다!
+        Write write = Write.builder()
+                .id(100)
+                .name("홍길동")
+                .content("안녕하세요")
+                .subject("hello")
+                .regDate(LocalDateTime.now())
+                .build();
+    return write;
+    }
+
+
+} // end controller
